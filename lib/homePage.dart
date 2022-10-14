d1b731df25bd619484d05b847ed05079099a4f05
ashikmhs <ashikmahmud113@gmail.com>
2022-10-14 17:53:17 +0600

    get api done

---
+++ lib/homePage.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_example/models/api_model.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<ApiModel> apiList = [];
+  Future<List<ApiModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = apiModelFromJson(response.body);
+    if (response.statusCode == 200) {
+      apiList = [...data];
+      return apiList;
+    } else {
+      return apiList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Expanded(
+          child: FutureBuilder(
+              future: getPostApi(),
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text('Loading.....');
+                } else {
+                  return ListView.builder(
+                      itemCount: apiList.length,
+                      itemBuilder: ((context, index) {
+                        return Text(apiList[index].title.toString());
+                      }));
+                }
+              })),
+        ),
+      ),
+    );
+  }
+}
